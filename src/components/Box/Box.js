348302af2276b1616698a707fdcfd742a42de9e1
Hannah Hudson <hannah.hch@gmail.com>
2020-11-11 21:06:37 -0700

    Add ability to remove cards

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -3,22 +3,31 @@ import React from 'react';
 
 const Box = (props) => {
   const [showSecretContent, setShowSecretContent] = React.useState(false);
+  const [hideBox, setHideBox] = React.useState(false);
 
   const handleBoxClick = () => {
     !showSecretContent ? setShowSecretContent(true) : setShowSecretContent(false);
   }
 
+  const handleButtonClick = () => {
+    !hideBox ? setHideBox(true) : setHideBox(false);
+  }
+  
   return (
-    <div className={
-      "box " + (showSecretContent ? " animated" : "")
-    } onClick={handleBoxClick}>
-      <a className="box-link" target="_blank" href={props.link}>Let's Go!</a>
+    <>
+      {!hideBox ? 
       <div className={
-        "box--inner " + (showSecretContent ? " animated" : "")
-      }>
-        <h2 className="box-title">{props.day}</h2>
-      </div>
-    </div>
+        "box " + (showSecretContent ? " animated" : "")
+      } onClick={handleBoxClick}>
+        <a className="box-link" target="_blank" href={props.link}>Let's Go!</a>
+        <button className="box-button" onClick={handleButtonClick}>Finished?</button>
+        <div className={
+          "box--inner " + (showSecretContent ? " animated" : "")
+        }>
+          <h2 className="box-title">{props.day}</h2>
+        </div>
+      </div> : null}
+    </>
   )
 }
 
