46b37bff0cdf6643700f24dc1ca7819f2fe9bd1f
Hannah Hudson <hannah.hch@gmail.com>
2020-11-16 17:31:58 -0500

    Cleaned up classes

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -3,31 +3,26 @@ import React from 'react';
 
 const Box = (props) => {
   const [showSecretContent, setShowSecretContent] = React.useState(false);
-  const [hideBox, setHideBox] = React.useState(false);
 
   const handleBoxClick = () => {
     !showSecretContent ? setShowSecretContent(true) : setShowSecretContent(false);
   }
 
-  const handleButtonClick = () => {
-    !hideBox ? setHideBox(true) : setHideBox(false);
+  const getBoxClasses = () => {
+    return showSecretContent ? "box animated" : "box"
   }
-  
-  return (
-    <>
-      {!hideBox ? 
-      <div className={
-        "box " + (showSecretContent ? " animated" : "")
-      } onClick={handleBoxClick}>
-        <a className="box-link" target="_blank" href={props.link}>Let's Go!</a>
-        <button className="box-button" onClick={handleButtonClick}>Finished?</button>
-        <div className={
-          "box--inner " + (showSecretContent ? " animated" : "")
-        }>
-          <h2 className="box-title">{props.day}</h2>
-        </div>
-      </div> : null}
-    </>
+
+  const getInnerBoxClasses = () => {
+    return showSecretContent ? "box--inner animated" : "box--inner"
+  }
+
+  return ( 
+    <div className={getBoxClasses()} onClick={handleBoxClick}>
+      <a className="box-link" target="_blank" href={props.link}>Let's Go!</a>
+      <div className={getInnerBoxClasses()}>
+        <h2 className="box-title">{props.day}</h2>
+      </div>
+    </div> 
   )
 }
 
