d75c13f5ac8363eff78e373a27cc6250effba4a2
Hannah Hudson <hannah.hch@gmail.com>
2020-11-17 22:04:28 -0500

    Removed snowflakes and font awesome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Boxes from './components/Boxes/Boxes';
-import Snowflakes from './components/Snowflakes/Snowflakes';
 
 function App() {
   return (
     <div className="App">
-      <Snowflakes/>
        <Boxes></Boxes>
     </div>
    
