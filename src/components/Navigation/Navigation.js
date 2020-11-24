532e2d744859f6e788b988757001b2f4b0bd219b
Hannah Hudson <hannah.hch@gmail.com>
2020-11-24 16:45:22 -0700

    Added beginning tooltip

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from '../Title/Title';
+import Tooltip from '../Tooltip/Tooltip';
 import './Navigation.css';
 
 const Navigation = () => {
   return (
     <nav className="navigation">
       <Title/>
+      <Tooltip/>
     </nav>
   )
 }
---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Tooltip.css';
+
+const Tooltip = () => {
+  return <div>?</div>
+}
+
+export default Tooltip;
