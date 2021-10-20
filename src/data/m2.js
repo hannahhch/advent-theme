35d1af7fe813e3564ad43b3dbf246c421f3ab19c
Hannah Hudson <hannah.hch@gmail.com>
2021-10-20 16:46:14 -0600

    Wipe M2 data file from filler

--- src/data/m2.js
+++ src/data/m2.js
@@ -1,46 +1,7 @@
 const modTwoData = {
   category: 'Frontend Fundamentals',
   days: [
-    {
-      link: "https://repl.it/@HannahHudson1/AdventDay1#index.js",
-      emoji: "🎁"
-    },
-    {
-      link: "https://codepen.io/hannahhch/pen/OJXGpxJ",
-      emoji: "✉️"
-    },
-    {
-      link: "https://github.com/turingschool-examples/ski-lift",
-      emoji: "⛷️"
-    },
-    {
-      link: "https://github.com/turingschool-examples/cookie-comp",
-      emoji: "🍪"
-    },
-    {
-      link: "https://repl.it/@HannahHudson1/Advent5#index.js",
-      emoji: "🥳"
-    },
-    {
-      link: "https://codepen.io/hannahhch/pen/QWEPeKb",
-      emoji: "❄️"
-    },
-    {
-      link: "https://codepen.io/hannahhch/pen/mdEYqjX",
-      emoji: "🕛"
-    },
-    {
-      link: "https://repl.it/@HannahHudson1/Advent8",
-      emoji: "🥋"
-    },
-    {
-      link: "https://github.com/turingschool-examples/do-you-wanna-build-a-snowman",
-      emoji: "☃️"
-    },
-    {
-      link: "https://github.com/turingschool-examples/winter-mad-libs",
-      emoji: "📝"
-    }
+    // TBD
   ]
 };
 
