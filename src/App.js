fe01f41e946bf8e35d10a8927e5aa1d0792b06c9
Hannah Hudson <hannah.hch@gmail.com>
2021-10-20 16:21:31 -0600

    Set up home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Boxes from './components/Boxes/Boxes';
 import Navigation from './components/Navigation/Navigation';
+import Home from './components/Home/Home';
 import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import modOneData from './data/m1.js';
 import modTwoData from './data/m2.js'
@@ -16,11 +16,14 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/m1">
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/m1">
             <Navigation title={modOneData.category}/>
             <Boxes data={modOneData}/>
           </Route>
-          <Route path="/m2">
+          <Route exact path="/m2">
             <Navigation title={modTwoData.category}/>
             <Boxes data={modTwoData} />
           </Route>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <Link to="/m1">M1</Link>
+      <Link to="/m2">M2</Link>
+    </>
+  )
+}
+
+export default Home;
