308d294d8cea044848611e7512c8c3a5cb9cf22a
Kayla Wood <kaylaewood@gmail.com>
2021-12-13 14:21:38 -0700

    Add m3 activities

--- src/data/m3.js
+++ src/data/m3.js
@@ -3,46 +3,41 @@ const modThreeData = {
   category: 'Frontend Frameworks',
   days: [
     {
-      link: "",
-      emoji: "🎉"
-    },
-    {
-      link: "https://replit.com/@kaylaewood/adventday2",
+      link: "https://replit.com/@kaylaewood/adventday1",
       emoji: "🎁"
     },
     {
-      link: "",
-      emoji: "⛷️"
+      link: "https://gist.github.com/kaylagordon/f5fd3e3ce34f63d5ca61e8f6f6ee8fa0",
+      emoji: "💰"
     },
     {
-      link: "",
-      emoji: "🍪"
+      link: "https://github.com/turingschool-examples/card-maker-cypress-tdd",
+      emoji: "💌"
     },
     {
-      link: "https://replit.com/@kaylaewood/adventday5",
-      emoji: "🍳"
-    },
-    {
-      link: "",
+      link: "https://gist.github.com/kaylagordon/e4df98dba4b33b04c2822a205e676d35",
       emoji: "❄️"
     },
     {
-      link: "",
-      emoji: "🕛"
-    },
-    {
-      link: "",
-      emoji: "🥋"
+      link: "https://github.com/turingschool-examples/responsive-css-imperfectfoods",
+      emoji: "🍎"
     },
     {
       link: "https://gist.github.com/kaylagordon/7d22b19c73e2bddfa6260c94f81ce26d",
-      emoji: "✉️"
+      emoji: "🍦"
     },
     {
-      link: "",
-      emoji: "📝"
-    }
+      link: "https://github.com/turingschool-examples/math4you-fe",
+      emoji: "🧮"
+    },
+    {
+      link: "https://gist.github.com/kaylagordon/a5477013405461e1820dfd4efcd09687",
+      emoji: "🧸"
+    },
   ]
+
 };
+
+
 
 export default modThreeData;
